2c65ef340ffebb854534a0fa2bd497e38055582b
alexjeman <alexandrujeman.ja@gmail.com>
2020-04-15 11:29:33 +0300

    Add settings file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -7,7 +8,9 @@ class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -16,6 +19,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
