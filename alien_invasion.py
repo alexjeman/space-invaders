424d02a7203b852c3b18f4d008aa6603c4e508c3
alexjeman <alexandrujeman.ja@gmail.com>
2020-04-18 12:47:53 +0300

    Limit bullets on the screen, bullet method

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,12 +28,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
             self._update_screen()
-
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
 
     def _check_events(self):
         """Respond to keypress and mouse events"""
@@ -63,8 +59,15 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
